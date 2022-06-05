018e4feb9ea6be7d0c44d7875f386003146a4571
ruancs <ruan.cs@hotmail.com>
2022-06-04 23:09:21 -0300

    inserindo numeros no display

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
 'use strict'
 
+const display = document.getElementById('display-calc');
+
+const numeros = document.querySelectorAll('[id*=key]');
+
+const atualizarDisplay = (texto) =>{
+    display.textContent += texto;
+}
+
+
+const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
+numeros.forEach (numero => numero.addEventListener('click', inserirNumero));
