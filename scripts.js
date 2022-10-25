edffc09ee869351fdf90c56d7284a1995952564c
ruancs <ruan.cs@hotmail.com>
2022-10-25 11:47:30 -0300

    adicionando funcao de operadores

--- scripts.js
+++ scripts.js
@@ -4,10 +4,29 @@ const display = document.getElementById('display-calc');
 
 const numeros = document.querySelectorAll('[id*=key]');
 
+const operadores = document.querySelectorAll('[id*=operador]');
+
+let novoNumero = true;
+
+let operador;
+
 const atualizarDisplay = (texto) =>{
-    display.textContent += texto;
+    if (novoNumero){
+        display.textContent = texto;
+        novoNumero = false;
+    }else{
+        display.textContent += texto;
+    }
+    
 }
 
 
 const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
+
 numeros.forEach (numero => numero.addEventListener('click', inserirNumero));
+
+const selecionarOperador = () =>{
+    novoNumero = true;
+}
+
+operadores.forEach (operador => operador.addEventListener('click', selecionarOperador));
